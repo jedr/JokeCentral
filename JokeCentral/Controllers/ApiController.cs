7c79ba6b1bdc2736676b0706fa2f344cab58534e
jedr <andrzej.stencel@gmail.com>
2020-10-09 14:50:42 +0200

    Add /api route as default, serving a sample joke

---
+++ JokeCentral/Controllers/ApiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JokeCentral.Controllers
+{
+    public class ApiController : Controller
+    {
+        public string Index()
+        {
+            return "You're not completely useless, you can always serve as a bad example.";
+        }
+    }
+}
--- JokeCentral/Startup.cs
+++ JokeCentral/Startup.cs
@@ -12,6 +12,7 @@ namespace JokeCentral
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,10 +27,9 @@ namespace JokeCentral
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Api}/{action=Index}/{id?}");
             });
         }
     }
