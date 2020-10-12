0c1326a61523a993392ab4ae0aa85417a30c9b52
jedr <andrzej.stencel@gmail.com>
2020-10-12 09:35:45 +0200

    Add OpenTelemetry ASP.NET Core instrumentation and console exporter

--- JokeCentral/Startup.cs
+++ JokeCentral/Startup.cs
@@ -1,8 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using OpenTelemetry.Trace;
 
 namespace JokeCentral
 {
@@ -13,6 +13,11 @@ namespace JokeCentral
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddOpenTelemetryTracing(builder =>
+                builder
+                    .AddAspNetCoreInstrumentation()
+                    .AddConsoleExporter()
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
