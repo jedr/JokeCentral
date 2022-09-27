571ab8ee8076fab7c593efaab42988bfc70793e1
jedr <andrzej.stencel@gmail.com>
2022-09-27 23:52:55 +0200

    feat: use multiple joke services simultaneously
    Return the first successfully retrieved joke.

--- JokeCentral.Api/JokeService.cs
+++ JokeCentral.Api/JokeService.cs
@@ -1,14 +1,43 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace JokeCentral.Api
 {
     public class JokeService : IJokeService
     {
+        private ChuckNorrisIo chuckNorrisIo = new ChuckNorrisIo();
         private IcndbCom icndbCom = new IcndbCom();
 
-        public Task<Joke> GetJokeAsync()
+        public async Task<Joke> GetJokeAsync()
         {
-            return this.icndbCom.GetJokeAsync();
+            var jokeServices = new List<IJokeService>();
+            jokeServices.Add(chuckNorrisIo);
+            jokeServices.Add(icndbCom);
+
+            var jokeCallsLinq = from jokeService in jokeServices select jokeService.GetJokeAsync();
+            var jokeCalls = jokeCallsLinq.ToList();
+
+            var jokes = new List<Joke>();
+            var errors = new List<Exception>();
+
+            while (jokeCalls.Any()) {
+                var completedCall = await Task.WhenAny(jokeCalls);
+                jokeCalls.Remove(completedCall);
+
+                if (completedCall.Exception != null) {
+                    errors.Add(completedCall.Exception);
+                } else {
+                    return completedCall.Result;
+                }
+            }
+        
+            if (errors.Any()) {
+                throw errors.First();
+            } else {
+                throw new Exception("This is awkward. Couldn't retrieve any joke, but didn't get any errors either.");
+            }
         }
     }
 }
